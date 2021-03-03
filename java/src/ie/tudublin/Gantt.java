a642ebe5863ae55117e5861594e1a448136cb4b2
cormac smith <cormacsmith@live.co.uk>
2021-03-03 17:13:31 +0000

    bla

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -1,23 +1,48 @@
 package ie.tudublin;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
+import processing.data.Table;
+import processing.data.TableRow;
 
 public class Gantt extends PApplet
 {	
+	ArrayList<Task> tasks = new ArrayList<Task>();
 	
 	public void settings()
 	{
 		size(800, 600);
 	}
 
+	public void displayTasks() {
+		colorMode(HSB);
+		float border = width * 0.1f;
+		for(int i = 1; i <= 30;i++ ) {
+			float x = map(i, 1, 30, border, width-border);
+			float y = map(i, 1, 30, border, height-border);
+			stroke(255);
+			fill(255);
+			line(x, y, x, y);
+
+		}
+	}
+
 	public void loadTasks()
 	{
-		
+		Table table = loadTable("tasks.csv", "header");
+		for(TableRow row : table.rows() ) {
+			// append to list 
+			Task task = new Task(row);
+			tasks.add(task);
+		}
 	}
 
 	public void printTasks()
 	{
-		
+		for(Task t : tasks) {
+			println(t);
+		}
 	}
 	
 	public void mousePressed()
@@ -34,6 +59,9 @@ public class Gantt extends PApplet
 	
 	public void setup() 
 	{
+		loadTasks();
+		printTasks();
+		displayTasks();
 	}
 	
 	public void draw()
